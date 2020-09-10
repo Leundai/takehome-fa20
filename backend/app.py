4c69e8b42f2b39a5e3f7af79607996c07c993825
repl.it user <replituser@example.com>
2020-09-10 03:44:11 +0000

    Backend Functionality Finished

--- backend/app.py
+++ backend/app.py
@@ -52,7 +52,48 @@ def mirror(name):
 
 @app.route("/restaurants", methods=['GET'])
 def get_all_restaurants():
-    return create_response({"restaurants": db.get('restaurants')})
+    restaurants = db.get('restaurants')
+    minRating = request.args.get('minRating')
+    filtered_restaurants = []
+
+    if minRating is None:
+        return create_response({"restaurants": restaurants})
+
+    # If this is not an int then return an error
+    try:
+        minRating = int(minRating)
+    except Exception as e:
+        # We can flash exception e here using Flask built in 
+        return create_response(status=404, message="minRating parameter is not an integer")
+
+    for restaurant in restaurants:
+        if restaurant['rating'] >= minRating:
+            filtered_restaurants.append(restaurant)
+    
+    return create_response({"restaurants": filtered_restaurants})
+
+@app.route("/restaurants", methods=['POST'])
+def add_restaurant():
+    # To check if there are even any name/rating
+    try:
+        name = request.form['name']
+        rating = request.form['rating']
+    except Exception as e:
+        return create_response(status=422, message="Missing parameters name or rating")
+    
+    # Assumption that we are only rating with integers
+    try:
+        rating = int(rating)
+    except Exception as e:
+        return create_response(status=422, message="Rating is not an integer")
+
+    if rating < 0 or rating > 10:
+        return create_response(status=422, message="Rating is not between 0 and 10")
+
+    payload = {'name': name, 'rating': rating}
+    # new restaurant might not be needed but it is clearer than payload
+    new_restaurant = db.create('restaurants', payload)
+    return create_response({'restaurant': new_restaurant}, status=201)
 
 @app.route("/restaurants/<id>", methods=['DELETE'])
 def delete_restaurant(id):
@@ -61,8 +102,42 @@ def delete_restaurant(id):
     db.deleteById('restaurants', int(id))
     return create_response(message="Restaurant deleted")
 
+@app.route("/restaurants/<id>", methods=['GET'])
+def get_restaurant(id):
+    try:
+        restaurant = db.getById('restaurants', int(id))
+    except Exception as e:
+        return create_response(status=422, message="Id is not an integer")
 
-# TODO: Implement the rest of the API here!
+    if restaurant is None:
+        return create_response(status=404, message="No restaurant with this id exists")
+    return create_response({"restaurant": restaurant})
+
+@app.route("/restaurants/<id>", methods=['PUT'])
+def change_restaurant(id):
+    try:
+        name = request.form['name']
+        rating = request.form['rating']
+    except Exception as e:
+        return create_response(status=422, message="Missing parameters name or rating")
+    
+    # Assumption that we are only rating with integers
+    # Also we could try to modularize this since it is the same in add_restaurant
+    try:
+        id = int(id)
+        rating = int(rating)
+    except Exception as e:
+        return create_response(status=422, message="Rating or Id is not an integer")
+
+    if rating < 0 or rating > 10:
+        return create_response(status=422, message="Rating is not between 0 and 10")
+    
+    payload = {'name': name, 'rating': rating}
+    restaurant = db.updateById('restaurants', id, payload)
+
+    if restaurant is None:
+        return create_response(status=404, message="No restaurant with this id exists")
+    return create_response({"restaurant": restaurant})
 
 """
 ~~~~~~~~~~~~ END API ~~~~~~~~~~~~
